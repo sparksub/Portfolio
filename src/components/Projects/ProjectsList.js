67d1e7872854fda67c7d536f42bd8d2b3180f442
Subin Park <lmhapy25@khu.ac.kr>
2023-01-05 15:10:49 +0900

    Update ProjectsList.js

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -11,7 +11,7 @@ export default [
     {
         title: "Educational VR for OA",
         imgPath: vr,
-        description: "We proposed VR content for fire evacuation training for the older adults (OA). We explored how OA navigate VR environments and what perception they have about VR. Moreover, we found how OA's behavior and perception differ from young adults. As a result, we suggested the design opportunities about VR as an educational material for disaster evacuation.\n\n As the first author, I led research and participated in all parts of the study other than VR development.",
+        description: "We proposed VR content for fire evacuation training for the older adults (OA). We explored how OA navigate VR environments and what perception they have about VR (with how to differ from young adults). As a result, we suggested the design opportunities for VR as an educational material for disaster evacuation.\n\n As the first author, I led research and participated in all parts of the study other than VR development.",
         isLink1: "paper",
         link1: "https://link.springer.com/article/10.1007/s10209-022-00878-8",
         isLink2: "paper",
@@ -20,7 +20,7 @@ export default [
     {
         title: "mHealth: Pillumi",
         imgPath: pillumi,
-        description: "It is difficult to easily identify a pill without its packaging or instructions. To solve this problem, we developed a pill search application using a hierarchical image classification model for 2500 Korean pills. \n\nI was responsible for developing cross-platform applications based on Flutter.",
+        description: "It's difficult to easily identify a pill without its packaging or instructions. To solve this problem, we developed a pill search app using a hierarchical image classification model for 2500 Korean pills. \n\nI was responsible for developing the cross platform app based on Flutter.",
         isLink1: "github",
         link1: "https://github.com/sparksub/pillumi-khu",
         isLink2: "",
@@ -29,7 +29,7 @@ export default [
     {
         title: "mHealth: Seesik",
         imgPath: seesik,
-        description: "It is very important for older adults (OA) suffering from chronic diseases to record when, how much and what they ate. However, this is cumbersome. We propose an AI-based food record & freshness evaluation app for the OA. A LiDAR sensor was used to measure the volume of food, allowing for more accurate nutrient analysis. \n\nI led the project and developed an iPad application and a volume measurement function using a LiDAR sensor.",
+        description: "It's important for older adults (OA) suffering from chronic diseases to record when, how much and what they ate. So, we propose an AI-based food record & freshness evaluation app for the OA. A LiDAR sensor was used to measure the volume of food, allowing for more accurate nutrient analysis. \n\nI led the project and developed an iPad app and a volume estimation using a LiDAR sensor.",
         isLink1: "video",
         link1: "https://www.youtube.com/watch?v=2KL2ZVbdMrA",
         isLink2: "",
@@ -38,7 +38,7 @@ export default [
     {
         title: "Surf",
         imgPath: surf,
-        description: "There were online learning platforms like internet lectures in the past, but no one would have imagined that all kinds of classes would be conducted non-face-to-face after pandemic. Our team developed Surf, an remote learning solution that can help reducing the gap between teachers and students during non-face-to-face class. \n\nI was responsible for developing web services based on React and SCSS.",
+        description: "There were online learning platforms like internet lectures in the past, but no one would have imagined that all kinds of classes would be conducted non-face-to-face after pandemic. Our team developed Surf, an remote learning solution that can help reducing the gap between teachers and students during non-face-to-face class. \n\nI was responsible for developing web services based on React.",
         isLink1: "video",
         link1: "https://youtu.be/6Ih8yFu74gQ",
         isLink2: "github",
@@ -47,16 +47,16 @@ export default [
     {
         title: "Redesigning Experiences of Student Volunteer Program",
         imgPath: sv,
-        description: "We report on an interview study with two groups, student volunteers (SV) and student volunteer chairs (SVC), to explore their experiences in contributing to an academic conference. We identified themes regarding their behaviors and perceptions when communicating with each other through an open coding approach. This is the first study that reports design opportunities to support SV and SVC. \n\nAlso, as the first author, I led research.",
-        isLink1: "video",
-        link1: "https://youtu.be/6Ih8yFu74gQ",
-        isLink2: "github",
-        link2: "https://github.com/FacerAin/2020-SW-Festival-PSY"
+        description: "We report on an interview study with two groups, 15 student volunteers (SV) and 12 student volunteer chairs (SVC), to explore their experiences in contributing to an conference. We identified themes regarding their behaviors and perceptions when communicating with each other through an open coding approach. \n\nAlso, I led research, as the first author.",
+        isLink1: "paper",
+        link1: "https://dl.acm.org/doi/abs/10.1145/3479599",
+        isLink2: "",
+        link2: ""
     },
     {
         title: "Toward accessible Live Streaming Platform",
         imgPath: streaming,
-        description: "The accessibility of live streaming for people with visual impairments (SVI) is still under-examined. Based on semi-structured interviews with 14 participants, we identified SVI's unique interactions and the challenges they face during live streaming. Based on the results of this study, we present design opportunities to better support SVI on live streaming platforms. \n\nIn this study, I analyzed the interview results using an affinity diagram, and designed design opportunities.",
+        description: "Based on semi-structured interviews with 14 participants, we identified the unique interactions and the challenges SVI face during live streaming, and present design opportunities to better support SVI on live streaming platforms. \n\nIn this study, I analyzed the interview results using an affinity diagram, and designed design opportunities.",
         isLink1: "paper",
         link1: "https://dl.acm.org/doi/abs/10.1145/3476038",
         isLink2: "",
@@ -65,7 +65,7 @@ export default [
     {
         title: "Toward Accessible C2C Market",
         imgPath: c2c,
-        description: "As the C2C market expands during the pandemic, the older adults (OA) are also starting to gain attention as new market participants. We developed a rule-based chatbot application (CSO, a C2C marketplace sales assistant for OA) to help the OA and asked OA's opinions. We expect the OA to be able to actively participate in the market as sellers of goods rather than as buyers. \n\nThis research is a Mercari R4D internship project, and I led the project and developed iOS application.",
+        description: "As the C2C market expands during the pandemic, the older adults (OA) was gain attention as new market participants. We developed a rule-based chatbot to help the OA. We expect the OA to be able to actively participate in the market as sellers of goods rather than as buyers. \n\nThis research was conducted as a Mercari R4D internship project, and I led the project and developed iOS app.",
         isLink1: "",
         link1: "",
         isLink2: "",
@@ -74,7 +74,7 @@ export default [
     {
         title: "Toward Accessible Daily Living",
         imgPath: smb,
-        description: "Little is known about the accessibility issues of people with visual impairments (PVI) with Seoul metropolitan buses. To reduce the gap, we conducted semi-structured interviews with nine PVI to investigate the challenges and opportunities for accessible SMB. Based on the identified challenges, we propose three opportunities for improving accessibility of the SMB system. \n\nAlso, as the first author, I led research.",
+        description: "Little is known about the accessibility issues of people with visual impairments (PVI) with Seoul metropolitan buses (SMB). To reduce the gap, we conducted semi-structured interviews with 9 PVI to investigate the challenges and opportunities for accessible SMB. \n\nAlso, I led research as the first author.",
         isLink1: "paper",
         link1: "https://dl.acm.org/doi/abs/10.1145/3391203.3391209",
         isLink2: "",
